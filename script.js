04ee3ec0e770d87e09f112f7b99d51ba9a55e031
hfizafarr <paracantik22@gmail.com>
2025-02-06 09:19:31 +0700

    p

---
+++ script.js
@@ -0,0 +1,6 @@
+// Fungsi untuk pindah ke halaman login setelah animasi selesai
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        window.location.href = 'login.html'; // Ganti 'login.html' dengan nama file halaman login Anda
+    }, 3000); // 3000 ms = 3 detik
+});
